8109ea52f07c5452cd35cc6973d7619bfa9eea4d
dedanirungu <dedanirungu@gmail.com>
2017-07-25 13:42:38 +0300

    Adding Bonus functionality

---
+++ Code/Models/BonusModel.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Bonus\Payments\Bonus\Code\Models;
+
+defined('KAZIST') or exit('Not Kazist Framework');
+
+use Kazist\KazistFactory;
+use Bonus\Payments\Code\Models\PaymentsModel;
+use Kazist\Service\Database\Query;
+
+/**
+ * Description of MarketplaceModel
+ *
+ * @author sbc
+ */
+class BonusModel extends PaymentsModel {
+
+    public $payment_code = '';
+
+    public function notificationTransaction($payment_id) {
+
+        $this->completeTransaction($payment_id);
+    }
+
+    public function completeTransaction($payment_id) {
+
+        $process_bonus = $this->processBonusPayment($payment_id);
+
+        if ($process_bonus) {
+            parent::successfulTransaction($payment_id, $this->payment_code);
+        } else {
+            parent::failTransaction($payment_id, $this->payment_code);
+        }
+    }
+
+    public function cancelTransaction($payment_id) {
+        parent::cancelTransaction($payment_id);
+    }
+
+    public function processBonusPayment($payment_id) {
+
+        $factory = new KazistFactory();
+
+        $payment = $this->getPaymentById($payment_id);
+        $deductions = json_decode($payment->deductions);
+        $this->payment_code = $payment->receipt_no;
+
+        $total_earning = $this->getUserTotalEarning($payment);
+
+        if ($total_earning >= $deductions->amount) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getUserTotalEarning($payment) {
+
+        $debit = $this->getUserTotalDebit($payment);
+        $credit = $this->getUserTotalCredit($payment);
+
+        return $credit - $debit;
+    }
+
+    public function getUserTotalDebit($payment) {
+
+        $factory = new KazistFactory();
+
+        $query = new Query();
+        $query->select('SUM(fg.debit) as total');
+        $query->from('#__bonus_transactions', 'fg');
+        $query->where('fg.user_id=:user_id');
+        $query->setParameter('user_id', $payment->user_id);
+
+
+        $record = $query->loadObject();
+
+        return $record->total;
+    }
+
+    public function getUserTotalCredit($payment) {
+        $factory = new KazistFactory();
+
+
+        $query = new Query();
+        $query->select('SUM(fg.credit) as total');
+        $query->from('#__bonus_transactions', 'fg');
+        $query->where('fg.user_id=:user_id');
+        $query->setParameter('user_id', $payment->user_id);
+
+
+        $record = $query->loadObject();
+
+        return $record->total;
+    }
+
+}
